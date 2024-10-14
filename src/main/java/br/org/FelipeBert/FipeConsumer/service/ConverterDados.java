3245d1b37e9a4898055162cdfe97c5b83445eae8
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-14 19:13:41 -0300

    Created data conversion service
    - Added IConverterDados interface and ConverterDados class to handle JSON data conversion using Jackson, enabling retrieval of individual objects and lists from API responses.

---
+++ src/main/java/br/org/FelipeBert/FipeConsumer/service/ConverterDados.java
@@ -0,0 +1,23 @@
+package br.org.FelipeBert.FipeConsumer.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
+
+public class ConverterDados implements IConverterDados{
+    private ObjectMapper mapper = new ObjectMapper();
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) throws JsonProcessingException {
+        return mapper.readValue(json, classe);
+    }
+
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) throws JsonProcessingException {
+        CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class, classe);
+
+        return mapper.readValue(json, lista);
+    }
+}
---
+++ src/main/java/br/org/FelipeBert/FipeConsumer/service/IConverterDados.java
@@ -0,0 +1,10 @@
+package br.org.FelipeBert.FipeConsumer.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.List;
+
+public interface IConverterDados {
+    <T> T obterDados(String json, Class<T> classe) throws JsonProcessingException;
+    <T> List<T> obterLista(String json, Class<T> classe) throws JsonProcessingException;
+}
